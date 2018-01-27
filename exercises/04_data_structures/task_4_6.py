4a16044070177a3859ba542d6ff951f4da5fbd13
ximapro <31029892+ximapro@users.noreply.github.com>
2018-01-27 23:38:32 +0300

    Update task_4_6.py

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -15,3 +15,15 @@ Outbound Interface    FastEthernet0/0
 '''
 
 ospf_route = 'O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
+ospf_route = ospf_route.split()
+ospf_route[0] = 'OSPF'
+ospf_route_template = '''               
+Protocol:\t\t{}
+Prefix:\t\t\t{}
+AD/Metric:\t\t{}
+Next-Hop:\t\t{}
+Last update:\t\t{}
+Outbound Interface:\t{}
+'''
+print(ospf_route_template.format(ospf_route[0], ospf_route[1], ospf_route[2], ospf_route[4], ospf_route[5], ospf_route[6]))
+
